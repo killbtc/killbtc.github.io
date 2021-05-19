0aa22879deb5255414486370e74a6afb1c159463
Alan Meira <alanmeira@gmail.com>
2021-05-19 20:16:30 -0300

    initial commit

---
+++ src/Main.jsx
@@ -0,0 +1,204 @@
+import React from "react";
+
+import killbtc from "./killbtc.png";
+
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="child">
+        <div className="title">
+          <img className="logo" src={killbtc} alt="killbtc" />
+          <h1>Kill BTC!</h1>
+          <p>Tesla will sell their Bitcoins?</p>
+          <p>We are going to buy it all with $KillBTC's! HODL!</p>
+          <h2>Contract: <b>0x36577bb306a6963892d3258dd0623f04faf85700</b></h2>
+          <br />
+          <a
+            className="mr-5"
+            href="https://t.me/joinchat/jDgZ6BkoU7ZmN2E8"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-telegram"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#9E9E9E"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
+            </svg>
+          </a>
+
+          <a
+            className="mr-5"
+            href="https://twitter.com/"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-twitter"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              strokeWidth="1"
+              stroke="#9E9E9E"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z"></path>
+              <path d="M22 4.01c-1 .49-1.98.689-3 .99-1.121-1.265-2.783-1.335-4.38-.737S11.977 6.323 12 8v1c-3.245.083-6.135-1.395-8-4 0 0-4.182 7.433 4 11-1.872 1.247-3.739 2.088-6 2 3.308 1.803 6.913 2.423 10.034 1.517 3.58-1.04 6.522-3.723 7.651-7.742a13.84 13.84 0 0 0 .497 -3.753C20.18 7.773 21.692 5.25 22 4.009z"></path>
+            </svg>
+          </a>
+
+          <a
+            className="mr-5"
+            href="https://www.reddit.com/"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-reddit"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#9E9E9E"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 8c2.648 0 5.028 .826 6.675 2.14a2.5 2.5 0 0 1 2.326 4.36c0 3.59 -4.03 6.5 -9 6.5c-4.875 0 -8.845 -2.8 -9 -6.294l-1 -.206a2.5 2.5 0 0 1 2.326 -4.36c1.646 -1.313 4.026 -2.14 6.674 -2.14z" />
+              <path d="M12 8l1 -5l6 1" />
+              <circle cx="19" cy="4" r="1" />
+              <circle cx="9" cy="13" r=".5" fill="currentColor" />
+              <circle cx="15" cy="13" r=".5" fill="currentColor" />
+              <path d="M10 17c.667 .333 1.333 .5 2 .5s1.333 -.167 2 -.5" />
+            </svg>
+          </a>
+
+          <a
+            className="mr-5"
+            href="https://discord.gg/"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-discord"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#9E9E9E"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="9" cy="12" r="1" />
+              <circle cx="15" cy="12" r="1" />
+              <path d="M7.5 7.5c3.5 -1 5.5 -1 9 0" />
+              <path d="M7 16.5c3.5 1 6.5 1 10 0" />
+              <path d="M15.5 17c0 1 1.5 3 2 3c1.5 0 2.833 -1.667 3.5 -3c.667 -1.667 .5 -5.833 -1.5 -11.5c-1.457 -1.015 -3 -1.34 -4.5 -1.5l-1 2.5" />
+              <path d="M8.5 17c0 1 -1.356 3 -1.832 3c-1.429 0 -2.698 -1.667 -3.333 -3c-.635 -1.667 -.476 -5.833 1.428 -11.5c1.388 -1.015 2.782 -1.34 4.237 -1.5l1 2.5" />
+            </svg>
+          </a>
+
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-brand-instagram"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#9e9e9e"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <rect x="4" y="4" width="16" height="16" rx="4" />
+              <circle cx="12" cy="12" r="3" />
+              <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+            </svg>
+          </a>
+        </div>
+
+        <div className="section">
+          <div>
+            <h3>Tokenomics:</h3>
+            <ul>
+              <li>1,000,000,000,000 Max supply</li>
+              <li>63% of supply burnt at launch.</li>
+              <li>All supply added to liquidity</li>
+              <li>Fair Launch</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Rules:</h3>
+            <ul>
+              <li>3% burnt every swap</li>
+              <li>3% distributed to holders</li>
+              <li>3% to dev & marketing</li>
+              <li>Use at least 9.5% slippage</li>
+            </ul>
+          </div>
+          <div>
+            <h3>Proofs:</h3>
+            <ul>
+              <a
+                href="https://bscscan.com/address/0x36577bb306a6963892d3258dd0623f04faf85700"
+                target="_blank"
+              >
+                <li>Contract Address</li>
+              </a>
+              <a
+                href="https://bscscan.com/tx/0x26c3bcbf8c43721e5bd95c51a5bd3700a1d887e0b5b31dedb7e05b71f033699b"
+                target="_blank"
+              >
+                <li>Locked liquidity</li>
+              </a>
+              <a
+                href="https://bscscan.com/tx/0x26c3bcbf8c43721e5bd95c51a5bd3700a1d887e0b5b31dedb7e05b71f033699b"
+                target="_blank"
+              >
+                <li>Burnt tokens</li>
+              </a>
+              <a
+                href="https://charts.bogged.finance/?token=0x36577BB306A6963892D3258DD0623F04FAf85700"
+                target="_blank"
+              >
+                <li>Bogged chart</li>
+              </a>
+
+              <a
+                href="https://www.rugscreen.com/scan/certificate?tokenid=1fb351ae19"
+                target="_blank"
+              >
+                <li>Rugscreen</li>
+              </a>
+              
+            </ul>
+          </div>
+        </div>
+
+        <div className="btn-action">
+          <a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x36577BB306A6963892D3258DD0623F04FAf85700" target="_blank">
+            <button className="buybtn">Buy $FUCKBTC</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
